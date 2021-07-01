c2d6481150e7128ea58848e74ee0b1cc1541f799
yoona610 <xiaotengxinai@gmail.com>
2021-07-01 10:25:32 +0000

    コメント機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 end
