2d7e65fbf9e3c2a63cbf2397bb477fafd1883765
yoona610 <xiaotengxinai@gmail.com>
2021-07-02 06:23:24 +0000

    フォロー/フォロワー機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    member do
+    get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
