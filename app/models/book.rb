7b518d87666e6d95f2334e6559d5ad2ef2543234
yoona610 <xiaotengxinai@gmail.com>
2021-07-06 04:41:07 +0000

    応用課題7a更新

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,13 +2,10 @@ class Book < ApplicationRecord
 	belongs_to :user
 	has_many :book_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
+	has_many :favorited_users, through: :favorites, source: :user
 
 	def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
-  end
-
-  def self.this_week
-  	Book.joins(:favorites).where(favorites:{created_at: Date.today.beginning_of_week.. Date.today.beginning_of_week(:sunday)}).group(:id).order("count(*) desc")
   end
 
   is_impressionable counter_cache: true
